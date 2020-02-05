9eef47b271f2248ffec32c9ccf99601d2ab24535
Brandon Philips <brandon@ifup.org>
2020-02-05 15:50:31 -0800

    main: respect PORT environment variable
    The go111 runtime should use the PORT variable to decide the listening
    http port.
    
    "PORT The port that receives HTTP requests."
    https://cloud.google.com/appengine/docs/standard/go111/runtime

--- main.go
+++ main.go
@@ -40,7 +40,12 @@ func main() {
 		log.Fatal(err)
 	}
 	http.Handle("/", h)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
